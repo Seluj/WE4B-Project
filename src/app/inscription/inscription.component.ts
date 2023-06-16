e4c7f42f3016107015a1f521d506401eec871200
jules <jules.ferlin@gmail.com>
2023-06-16 23:00:03 +0200

    modification de la page d'inscription

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl, FormGroup,
+  ValidationErrors, ValidatorFn, Validators,
+  ReactiveFormsModule
+} from "@angular/forms";
+import {CustomValidators} from "../custom-validators";
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.css']
+  styleUrls: ['./inscription.component.scss']
 })
-export class InscriptionComponent {
+export class InscriptionComponent implements OnInit {
+
+  inscriptionForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    pwd: new FormControl('', [
+      Validators.required,
+
+      CustomValidators.patternValidator(/\d/, {hasNumber: true}),
+
+      CustomValidators.patternValidator(/[A-Z]/, {hasCapitalCase: true}),
+
+      CustomValidators.patternValidator(/[a-z]/, {hasSmallCase: true}),
+
+      CustomValidators.patternValidator(/[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {hasSpecialCharacters: true}),
+
+      Validators.minLength(8),
+
+      CustomValidators.matchValidator('pwd2', true)
+    ]),
+    pwd2: new FormControl('', [
+      Validators.required,
+      CustomValidators.matchValidator('pwd')
+    ]),
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required)
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onInscriptionForm() {
+    console.log(this.inscriptionForm.value);
+  }
 
 }
