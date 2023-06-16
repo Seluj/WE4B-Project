af2b9606ece0123fbe20a8f732ba126b56229dfe
jules <jules.ferlin@gmail.com>
2023-06-16 22:56:51 +0200

    ajout de condition pour l'affichage des elements du haut et du bas de la page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router, RouterEvent} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WE4B-Project';
+  currentUrl: string | undefined;
+
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof RouterEvent) {
+        this.currentUrl = event.url;
+      }
+    });
+  }
 }
