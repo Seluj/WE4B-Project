547be7f4d16c512b4576c1eb1a728fb992fa2bd1
Céliane <celianeallaire@gmail.com>
2023-06-17 12:46:17 +0200

    on récupère l'id envoyé par le component carte

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Restaurant } from '../models/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-restaurant',
@@ -9,11 +10,21 @@ import { Restaurant } from '../models/restaurant.model';
 export class RestaurantComponent implements OnInit {
   @Input() restaurant!: Restaurant;
 
+  restaurantId!: string;
   liked!: boolean;
   class!: string;
 
+  constructor(private route: ActivatedRoute) { }
+
   ngOnInit(): void {
-    this.restaurant = new Restaurant("BOB", "5 rue du BOB", "../../assets/restaurant_belfort.jpg", "wow trop bien BOB", 0, 0, 0);
+    this.route.params.subscribe(params => {
+      this.restaurantId = params['id'];
+    });
+    if (this.restaurantId === "1") {
+      this.restaurant = new Restaurant("BOB", "5 rue du BOB", "../../assets/restaurant_belfort.jpg", "wow trop bien BOB", 0, 0, 0);
+    } else if (this.restaurantId === "2") {
+      this.restaurant = new Restaurant("JIMBOB", "5 rue du JIMBOB", "../../assets/restaurant_berlin.jpg", "wow trop bien JIMBOB", 0, 0, 0);
+    }
     this.liked = false;
     this.class = "like";
   }
