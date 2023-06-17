34332cd9ead9d9bc4099550af008068d624db985
Céliane <celianeallaire@gmail.com>
2023-06-17 11:36:09 +0200

    updated Restaurantcomponent

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Restaurant } from '../models/restaurant.model';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrls: ['./restaurant.component.css']
 })
-export class RestaurantComponent {
+export class RestaurantComponent implements OnInit {
+  @Input() restaurant!: Restaurant;
+
+  liked!: boolean;
+  class!: string;
+
+  ngOnInit(): void {
+    this.liked = false;
+    this.class = "like";
+  }
+
+  onLike() {
+    if (this.liked === true) {
+      this.restaurant.popularite--;
+      this.liked = false;
+      this.class = "like";
+    } else {
+      this.restaurant.popularite++;
+      this.liked = true;
+      this.class = "unlike";
+    }
+  }
 
 }
