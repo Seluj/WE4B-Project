772e86874d83593f93836f9d2ca0c4f5f8ae9a82
jules <jules.ferlin@gmail.com>
2023-06-17 12:05:37 +0200

    suppression de choses inutilisées

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { CustomValidators } from "../custom-validators";
 
 @Component({
