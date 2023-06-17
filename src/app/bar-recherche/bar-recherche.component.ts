6a3917fcbc7d1f0c8a5cd8db8831f44423b2a2cd
Céliane <celianeallaire@gmail.com>
2023-06-17 16:11:14 +0200

    Début de la barre de recherche

--- src/app/bar-recherche/bar-recherche.component.ts
+++ src/app/bar-recherche/bar-recherche.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+interface SearchCriteria {
+  prix: {
+    cheap: boolean;
+    moderate: boolean;
+    expensive: boolean;
+  };
+  adresse: string;
+  type: {
+    type1: boolean;
+    type2: boolean;
+    type3: boolean;
+  };
+}
+
 @Component({
   selector: 'app-bar-recherche',
   templateUrl: './bar-recherche.component.html',
   styleUrls: ['./bar-recherche.component.css']
 })
-export class BarRechercheComponent {
 
+export class BarRechercheComponent {
+  searchCriteria: SearchCriteria = {
+    prix: {
+      cheap: false,
+      moderate: false,
+      expensive: false
+    },
+    adresse: '',
+    type: {
+      type1: false,
+      type2: false,
+      type3: false
+    }
+  }
+  rechercheRestaurant() {}
 }
