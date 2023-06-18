2e52b72e859646f2323f0871a0f9f88e40c3480f
jules <jules.ferlin@gmail.com>
2023-06-18 17:06:30 +0200

    suppression de choses inutilisées

--- src/app/restaurants.service.ts
+++ src/app/restaurants.service.ts
@@ -1,7 +1,7 @@
-import {Injectable, OnInit} from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {map} from "rxjs";
-import {Restaurant} from "./models/restaurant.model";
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs";
+import { Restaurant } from "./models/restaurant.model";
 
 @Injectable({
   providedIn: 'root'
