bb44bb7a06a295402e9f6ad8418b5c7707fa739f
jules <jules.ferlin@gmail.com>
2023-06-18 16:51:49 +0200

    implémentation nav-bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterEvent} from "@angular/router";
+import {UtilisateurService} from "../utilisateur.service";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   currentUrl: string | undefined;
+  connected: boolean = false;
+  prenom!: string;
+  nom!: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private serviceUtilisateur: UtilisateurService) {
     router.events.subscribe(event => {
       if (event instanceof RouterEvent) {
         this.currentUrl = event.url;
       }
     });
+  }
+
+  ngOnInit(): void {
+    if (this.serviceUtilisateur.estConnecte()) {
+      this.connected = true;
+      this.prenom = <string>this.serviceUtilisateur.getSessionItem('prenom');
+      this.nom = <string>this.serviceUtilisateur.getSessionItem('nom');
+    } else {
+      this.connected = false;
+    }
+  }
+
+  onDeconnexion() {
+    this.serviceUtilisateur.deleteSession();
+    this.router.navigate(['/']);
+    window.location.reload();
   }
 }
