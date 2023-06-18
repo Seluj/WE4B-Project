0eb92cda4dc3d2922668c3fa59bacc6477b8f89d
Céliane <celianeallaire@gmail.com>
2023-06-18 15:57:15 +0200

    changed routing method

--- src/app/bar-recherche/bar-recherche.component.ts
+++ src/app/bar-recherche/bar-recherche.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 interface SearchCriteria {
   prix: {
@@ -33,6 +34,24 @@ export class BarRechercheComponent {
       type2: false,
       type3: false
     }
+  };
+
+  constructor(private router: Router) { }
+
+
+  rechercheRestaurant() {
+    const { cheap, moderate, expensive } = this.searchCriteria.prix;
+    const { type1, type2, type3 } = this.searchCriteria.type;
+    const { adresse } = this.searchCriteria;
+
+    this.router.navigate(['recherche',
+      cheap.toString(),
+      moderate.toString(),
+      expensive.toString(),
+      adresse,
+      type1.toString(),
+      type2.toString(),
+      type3.toString()
+    ]);
   }
-  rechercheRestaurant() {}
 }
